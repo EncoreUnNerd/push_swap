4ee0f81a8d737152758ea839d04713cbaab9204f
EncoreUnNerd <mathys.n1@protonmail.com>
2024-11-29 12:33:08 +0100

    norm

--- push_swap.h
+++ push_swap.h
@@ -1,8 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhenin <mhenin@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 12:31:03 by mhenin            #+#    #+#             */
+/*   Updated: 2024/11/29 12:32:08 by mhenin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "libft/libft.h"
-#include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
 
 #endif
